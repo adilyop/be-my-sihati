364fb2ec6f7e098d10986c3e6d8a1812d36f3f94
aguezzi <guezziadil@gmail.com>
2022-01-31 21:35:24 +0100

    add patient information

--- server/models/patients.js
+++ server/models/patients.js
@@ -8,6 +8,7 @@ const patientsSchema = new Schema({
   first_name: String,
   last_name: String,
   phone_number: String,
+  dob: String,
   email_address: String,
   ssn: String,
   discharge_status: { type: String, default: 'NotScheduled', enum: ['Scheduled', 'NotScheduled'] },
